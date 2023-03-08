4dc69eea5d2ee69018b5afbf1463f4d2875eab6c
Nastacia Shershova <znastacia@gmail.com>
2023-03-08 15:53:03 -0500

    added JS code

--- Develop/script.js
+++ Develop/script.js
@@ -1,23 +1,78 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-});
+ // update time
+function updateTime() {
+  var today = dayjs(); 
+
+// updates the time element in the header
+  $("#currentDay").text(today.format("dddd, MMMM DD YYYY, h:mm.ss"));
+
+//past, present, and future time blocks
+  let now = dayjs().format("hh");
+  for (let i = 0; i < scheduleElArray.length; i++) {
+      scheduleElArray[i].removeClass("future past present");
+
+      if (now > scheduleElArray[i].data("hour")) {
+          scheduleElArray[i].addClass("past");
+
+      } else if (now === scheduleElArray[i].attr("data-hour")) {
+          scheduleElArray[i].addClass("present");
+
+      } else {
+
+          scheduleElArray[i].addClass("future");
+      }
+  }
+}
+
+// textarea elements
+let saveBtn = $(".saveBtn");
+let containerEl = $(".container");
+let schedule9am = $("#hour-9");
+let schedule10am = $("#hour-10");
+let schedule11am = $("#hour-11");
+let schedule12pm = $("#hour-12");
+let schedule1pm = $("#hour-13");
+let schedule2pm = $("#hour-14");
+let schedule3pm = $("#hour-15");
+let schedule4pm = $("#hour-16");
+let schedule5pm = $("#hour-17");
+
+let scheduleElArray = [
+  schedule9am,
+  schedule10am,
+  schedule11am,
+  schedule12pm,
+  schedule1pm,
+  schedule2pm,
+  schedule3pm,
+  schedule4pm,
+  schedule5pm,
+];
+
+renderLastRegistered();
+updateTime();
+setInterval(updateTime, 1000); 
+
+function renderLastRegistered() {
+  for (let el of scheduleElArray) {
+      el.val(localStorage.getItem("time-block " + el.data("hour")));
+
+  }
+}
+
+
+// Click events
+function handleFormSubmit(event) {
+  event.preventDefault();
+
+  let btnClicked = $(event.currentTarget);
+
+  let targetText = btnClicked.siblings("textarea");
+
+  let targetTimeBlock = targetText.data("hour");
+
+  localStorage.setItem("time block " +  targetTimeBlock, targetText.val());
+}
+
+saveBtn.on("click", handleFormSubmit);
+
+
