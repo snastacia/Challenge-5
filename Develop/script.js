f7b22d831e312bf0ccd6264e753943e1b531a2fd
Nastacia Shershova <znastacia@gmail.com>
2023-03-09 23:08:32 -0500

    fixed background function

--- Develop/script.js
+++ Develop/script.js
@@ -8,10 +8,8 @@ $(function () {
     localStorage.setItem(time, userInput);
   });
 
-  // dayjs variable for current hour
-  var hour = dayjs().hour();
-
   //sets saved user data input to that of the text area
+
   $(".time-block").each(function () {
     var chosenTimeBlock = $(this)
     var id = chosenTimeBlock.attr("id")
@@ -24,22 +22,17 @@ $(function () {
   $("#currentDay").text(currentDate.format("dddd, MMMM DD, YYYY"))
 });
 
-// text area elements
-let schedule9am = $("#hour-9");
-let schedule10am = $("#hour-10");
-let schedule11am = $("#hour-11");
-let schedule12pm = $("#hour-12");
-let schedule1pm = $("#hour-13");
-let schedule2pm = $("#hour-14");
-let schedule3pm = $("#hour-15");
-let schedule4pm = $("#hour-16");
-let schedule5pm = $("#hour-17");
 
 //function to change background based on the hour
 function background () {
+
+// dayjs variable for current hour
+var hour = dayjs().hour();
+console.log(hour, "this is the time!");
       
   $(".description").each(function () {
       var timeTest = parseInt($(this).attr("id"));
+      console.log(timeTest);
       hour = parseInt(hour);
       console.log(timeTest);
       console.log(hour);
@@ -52,3 +45,5 @@ function background () {
       }
   });
 }
+
+background();
